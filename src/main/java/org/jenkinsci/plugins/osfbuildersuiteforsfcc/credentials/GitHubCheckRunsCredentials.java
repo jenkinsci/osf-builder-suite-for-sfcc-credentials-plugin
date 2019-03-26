d9a8b6894905794d2fbc72e771c43aac27946dc0
Daniel Anechitoaie <daniel.anechitoaie@gmail.com>
2019-03-26 13:51:50 +0200

    Added GitHubCheckRunsCredentials

---
+++ src/main/java/org/jenkinsci/plugins/osfbuildersuiteforsfcc/credentials/GitHubCheckRunsCredentials.java
@@ -0,0 +1,31 @@
+package org.jenkinsci.plugins.osfbuildersuiteforsfcc.credentials;
+
+import com.cloudbees.plugins.credentials.CredentialsNameProvider;
+import com.cloudbees.plugins.credentials.NameWith;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
+import hudson.util.Secret;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Nonnull;
+
+@NameWith(GitHubCheckRunsCredentials.NameProvider.class)
+public interface GitHubCheckRunsCredentials extends StandardCredentials {
+    @SuppressWarnings("unused")
+    String getAppId();
+
+    @SuppressWarnings("unused")
+    Secret getPrivateKey();
+
+    class NameProvider extends CredentialsNameProvider<GitHubCheckRunsCredentials> {
+        @Nonnull
+        @Override
+        public String getName(@Nonnull GitHubCheckRunsCredentials credentials) {
+            String description = StringUtils.trim(credentials.getDescription());
+            if (StringUtils.isNotEmpty(description)) {
+                return description;
+            }
+
+            return credentials.getId();
+        }
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/osfbuildersuiteforsfcc/credentials/impl/GitHubCheckRunsCredentialsImpl.java
@@ -0,0 +1,54 @@
+package org.jenkinsci.plugins.osfbuildersuiteforsfcc.credentials.impl;
+
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
+import hudson.Extension;
+import hudson.Util;
+import hudson.util.Secret;
+import org.jenkinsci.plugins.osfbuildersuiteforsfcc.credentials.GitHubCheckRunsCredentials;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
+public class GitHubCheckRunsCredentialsImpl extends BaseStandardCredentials implements GitHubCheckRunsCredentials {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String appId;
+    private final Secret privateKey;
+
+    @DataBoundConstructor
+    public GitHubCheckRunsCredentialsImpl(
+            @CheckForNull CredentialsScope scope,
+            @CheckForNull String id,
+            @CheckForNull String description,
+            @CheckForNull String appId,
+            @CheckForNull String privateKey) {
+
+        super(scope, id, description);
+
+        this.appId = Util.fixNull(appId);
+        this.privateKey = Secret.fromString(privateKey);
+    }
+
+    @Override
+    public String getAppId() {
+        return appId;
+    }
+
+    @Override
+    public Secret getPrivateKey() {
+        return privateKey;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BaseStandardCredentialsDescriptor {
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return "OSF Builder Suite :: GitHub Check Runs Credentials";
+        }
+    }
+}
